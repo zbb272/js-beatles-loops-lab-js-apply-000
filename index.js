d2f8660fb3e87a5624760bb1109fb358bf28ec61
Zachary <zbberman@yahoo.com>
2019-01-14 21:30:58 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 function theBeatlesPlay(musicians, instruments){
   var newArray = [];
   
-  for(i = 0; i < musicians.length; i++){
+  for(var i = 0; i < musicians.length; i++){
     newArray[i] = musicians[i] + " plays " + instruments[i];
   }
   
